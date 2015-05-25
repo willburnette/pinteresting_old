ef7db85a974e4b478fd80ebea0f3b93693e8618b
Will Burnette <will@hsdesignhouse.com>
2015-05-24 20:46:26 -0500

    Load pins in index action in reverse chronological order

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,8 +4,8 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all
-  end
+   @pins = Pin.all.order("created_at DESC")
+ end
 
   def show
   end
